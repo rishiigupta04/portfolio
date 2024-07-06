b781eb4af24f6b1ffe7728621b2bc5a49770d205
Rishhh <grishi349@gmail.com>
2024-07-06 23:28:13 +0530

    hero section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Rishi's Portfolio",
-  description: "Modern and Minimalist Portfolio",
+  description: "A Modern and Minimalist Portfolio",
 };
 
 export default function RootLayout({
@@ -16,7 +17,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
