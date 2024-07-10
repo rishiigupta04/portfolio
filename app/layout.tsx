7bf5aad1740c4eddbc94731706aa1daa11399e57
Rishhh <grishi349@gmail.com>
2024-07-10 15:59:03 +0530

    added locomotive scrolling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "./theme-provider";
+import RootLayoutClient from "./RootLayoutClient";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,16 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
+      <RootLayoutClient interClass={inter.className}>
+        {children}
+      </RootLayoutClient>
     </html>
   );
 }
