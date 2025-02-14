da929653ab73dff6289ba2db58fe76e7c63f455a
Rishiraj Gupta <grishi349@gmail.com>
2025-02-14 22:02:22 +0530

    some changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -24,9 +24,9 @@ const Hero = () => {
       </div>
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-100">
+          {/* <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-100">
             Powered by Next.JS, Typescript & Sentry
-          </h2>
+          </h2> */}
 
           <TextGenerateEffect
             className="text-center text-[42px] md:text-5xl lg:text-[70px] "
